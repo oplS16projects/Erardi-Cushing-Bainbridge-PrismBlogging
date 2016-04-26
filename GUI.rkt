3e4bc411388031a3094e242442be321084acea4d
alex <alexander_cushing@student.uml.edu>
2016-04-25 21:14:43 -0400

    new gui fixes

--- GUI.rkt
+++ GUI.rkt
@@ -3,13 +3,13 @@
 (require db)
 
 (define pgc
-    (postgresql-connect
-     #:server "ec2-54-235-64-239.compute-1.amazonaws.com"
-     #:port 5432
-     #:user "wnzhkkaivwijul"
-     #:database "dbu5lo07i41k3g"
-     #:password "xF6aT4Dz6iBEyxEywvli5Tm1hN"
-     #:ssl 'yes))
+  (postgresql-connect
+   #:server "ec2-54-235-64-239.compute-1.amazonaws.com"
+   #:port 5432
+   #:user "wnzhkkaivwijul"
+   #:database "dbu5lo07i41k3g"
+   #:password "xF6aT4Dz6iBEyxEywvli5Tm1hN"
+   #:ssl 'yes))
 (define testTitle "I HOPE THIS WORKED")
 (define testBody "THIS SHOULD BE THE BODY") ;;these variables are placeholders for what will be filled with user entered data
 
@@ -24,33 +24,33 @@
   (display "\n")
   (createBlogPost title-new-value new-value))
 
-
 (define blogPost%
   (class horizontal-panel%
     (super-new)
-    (define (callback button event)
-      (define title-new-value (send titleoutput get-value))
-      (define new-value (send output get-value))
-      (save title-new-value new-value))
-      ;;(display title-new-value)
-      ;;(display new-value))
-    (define button (new button% (label "Submit")
-                        (vert-margin 0)
-                        (parent this)
-                        (callback callback)))
     (define titleoutput (new text-field% (label "    title")
                              (min-height 20)
                              (min-width 200)
                              (parent this)))
     (define output (new text-field% (label "blog")
                         (style '(multiple))
-                        (min-height 450)
+                        (min-height 100)
                         (min-width 400)
-                        (stretchable-width 300)
-                        (vert-margin 0)
                         (parent this)))
+    (define (callback button event)
+      (define title-new-value (send titleoutput get-value))
+      (define new-value (send output get-value))
+      (save title-new-value new-value)
+      (send output set-value "")
+      (send titleoutput set-value "")
+      (send howisit show #t))
+    (define button (new button% (label "Submit")
+                        (vert-margin 0)
+                        (parent this)
+                        (callback callback)))
     ))
 
+(define howisit (new frame% (label "success")
+                     (min-width 400)))
 
 (define f (new frame% (label "prism blog post GUI") (min-width 400) (min-height 500)))
 
