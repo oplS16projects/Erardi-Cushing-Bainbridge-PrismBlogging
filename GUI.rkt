832527a187d6116953ac7158763ad9a13bd1510f
Alex Cushing <alexander_cushing@student.uml.edu>
2016-04-27 10:36:43 -0400

    Update GUI.rkt

--- GUI.rkt
+++ GUI.rkt
@@ -25,41 +25,51 @@
   (createBlogPost title-new-value new-value))
 
 (define blogPost%
-  (class horizontal-panel%
+  (class object% ; object% instead of horizontal-panel%
+    ; This argument is explicit now.
+    ; If other code relies on other arguments, specify them here.
+    (init parent)
     (super-new)
-    (define titleoutput (new text-field% (label "    title")
-                             (min-height 20)
-                             (min-width 200)
-                             (vert-margin 20)
-                             (horiz-margin 10)
-                             (parent this)))
-    (define output (new text-field% (label "blog")
-                        (style '(multiple))
-                        (min-height 20)
-                        (vert-margin 20)
-                        (min-width 400)
-                        (parent this)))
+    (define panel
+      (new horizontal-panel% ; this new call is explicit now
+           [parent parent]   ; you can later add more arguments
+           [alignment '(left top)])) ; and it won't break things
+    (define titleoutput
+      (new text-field%
+           [label "    title"]
+           [min-height 20]
+           [min-width 200]
+           [vert-margin 20]
+           [horiz-margin 10]
+           [parent panel])) ; panel instead of this
+    (define output
+      (new text-field%
+           [label "blog"]
+           [style '(multiple)]
+           [min-height 20]
+           [vert-margin 20]
+           [min-width 400]
+           [parent panel])) ; panel instead of this
     (define (callback button event)
       (define title-new-value (send titleoutput get-value))
       (define new-value (send output get-value))
       (save title-new-value new-value)
       (send output set-value "")
-      (send titleoutput set-value "")
-      (send howisit show #t))
-    (define button (new button% (label "Submit")
-                        (vert-margin 0)
-                        (horiz-margin 10)
-                        (parent this)
-                        (callback callback)))
+      (send titleoutput set-value "SUCCESS"))
+      ;(send howisit show #t))
+    (define button
+      (new button%
+           [label "Submit"]
+           [vert-margin 0]
+           [horiz-margin 10]
+           [parent panel] ; panel instead of this
+           [callback callback]))
     ))
 
-(define howisit (new frame% (label "success")
-                     (min-width 400)))
-
-(define f (new frame% (label "prism blog post GUI") (min-width 400) (min-height 500)))
+(define f (new frame% [label "blog post GUI"] [min-width 400] [min-height 500]))
 
 (define tib (new blogPost%
-                 (parent f)))
+                 [parent f]))
 
 (send f show #t)
 
